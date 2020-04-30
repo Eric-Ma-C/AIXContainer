59129b6eb25b9660beca2c740ed0f81d69938712
EricMa <eric315@qq.com>
2020-04-30 23:21:22 +0800

    初步添加http异步下载功能

---
+++ Client/src/main/java/zju/vipa/aix/container/client/netty/HttpClientInboundHandler.java
@@ -0,0 +1,32 @@
+package zju.vipa.aix.container.client.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpResponse;
+
+/**
+ * @Date: 2020/4/30 10:25
+ * @Author: EricMa
+ * @Description: 处理http Server响应
+ */
+public class HttpClientInboundHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        if (msg instanceof HttpResponse)
+        {
+            HttpResponse response = (HttpResponse) msg;
+            System.out.println("CONTENT_TYPE:" + response.headers().get(HttpHeaderNames.CONTENT_TYPE));
+        }
+        if(msg instanceof HttpContent)
+        {
+            HttpContent content = (HttpContent)msg;
+            ByteBuf buf = content.content();
+            System.out.println(buf.toString(io.netty.util.CharsetUtil.UTF_8));
+            buf.release();
+        }
+    }
+}
---
+++ Client/src/main/java/zju/vipa/aix/container/client/netty/HttpsDownloadHandler.java
@@ -0,0 +1,13 @@
+package zju.vipa.aix.container.client.netty;
+
+/**
+ * @Date: 2020/4/30 15:29
+ * @Author: EricMa
+ * @Description: todo:
+ */
+public class HttpsDownloadHandler extends HttpDownloadHandler {
+    public HttpsDownloadHandler(String savePath) {
+        super(savePath);
+    }
+
+}
